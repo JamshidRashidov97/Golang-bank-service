3f3e2c8ce854576a9e2dbb969b77c0cc5fe56e5e
Jamshid-Rashidov <rashidovj10@gmail.com>
2025-03-03 20:20:52 +0500

    add logs

--- simplebank/db/sqlc/store_test.go
+++ simplebank/db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,6 +13,7 @@ func TestTranferTx(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer transactions
 	n := 5
@@ -84,6 +86,7 @@ func TestTranferTx(t *testing.T) {
 		require.Equal(t, account2.ID, account1.ID)
 
 		// check accounts' balance
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
 		require.Equal(t, diff1, diff2)
@@ -103,6 +106,7 @@ func TestTranferTx(t *testing.T) {
 	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
 	require.NoError(t, err)
 
+	fmt.Println(">> after:", updateAccount1.Balance, updateAccount2.Balance)
 	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
 	require.Equal(t, account2.Balance-int64(n)*amount, updateAccount2.Balance)
 }
