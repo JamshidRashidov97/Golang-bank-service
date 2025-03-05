eae166f65fac8e8515361c10959a2b9e967c2e28
Jamshid-Rashidov <rashidovj10@gmail.com>
2025-03-05 19:43:48 +0500

    decrease the number of concurrent transfer transactions

--- simplebank/db/sqlc/store_test.go
+++ simplebank/db/sqlc/store_test.go
@@ -16,14 +16,16 @@ func TestTranferTx(t *testing.T) {
 	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer transactions
-	n := 5
+	n := 2
 	amount := int64(10)
 
 	errs := make(chan error)
 	results := make(chan TranferTxResult)
 	for i := 0; i < n; i++ {
+		txName := fmt.Sprintf("tx %d", i+1)
 		go func() {
-			result, err := store.TranferTx(context.Background(), TranferTxParams{
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			result, err := store.TranferTx(ctx, TranferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
